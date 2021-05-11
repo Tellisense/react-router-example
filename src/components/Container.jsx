0fe13f256d2ee4b5442d7991517d3b40ed7410c9
Justin Rahimi <9245522+Tellisense@users.noreply.github.com>
2021-05-11 15:41:41 -0700

    added colors

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Container = ({ children }) => {
   return (
-    <div className="container mx-auto pt-12 h-full">
+    <div className="container mx-auto pt-12 h-screen">
       {children}
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
         <div className="hidden md:flex md:items-center md:w-auto w-full order-3 md:order-1" id="menu">
           <nav>
-            <ul className="md:flex items-center justify-between text-base text-red-600 pt-4 md:pt-0">
+            <ul className="md:flex items-center justify-between text-base text-red-700 pt-4 md:pt-0">
               <li> <Link className="inline-block no-underline hover:text-black font-medium text-lg py-1 px-4 lg:-ml-2" to='/'>Home</Link></li>
               <li>  <Link className="inline-block no-underline hover:text-black font-medium text-lg py-1 px-4 lg:-ml-2" to='/products'>Products</Link></li>
               <li>  <Link className="inline-block no-underline hover:text-black font-medium text-lg py-1 px-4 lg:-ml-2" to='/posts/2018/06'>Posts</Link></li>
@@ -20,7 +20,7 @@ const Navbar = () => {
         <div className="order-2 md:order-3 flex flex-wrap items-center justify-end mr-0 md:mr-4" id="nav-content">
           <div className="auth flex items-center w-full md:w-full">
             <button className="bg-transparent text-gray-800  px-2 py-1 rounded border border-gray-300 mr-4 hover:bg-gray-100 hover:text-gray-700">Sign in</button>
-            <button className="bg-red-600 text-gray-200  px-2 py-1 rounded  hover:bg-red-500 hover:text-gray-100">Sign up</button>
+            <button className="bg-red-700 text-gray-200  px-2 py-1 rounded  hover:bg-red-400 hover:text-gray-100">Sign up</button>
           </div>
         </div>
       </div>
